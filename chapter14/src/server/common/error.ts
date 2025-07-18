fc1ff097bbea789caf2aca0ca7b3441a09b21861
fredafei <964959033@qq.com>
2025-07-18 14:26:08 +0800

    调整验证数据合法性的数据响应格式为统一的错误处理的格式

--- chapter14/src/server/common/error.ts
+++ chapter14/src/server/common/error.ts
@@ -1,6 +1,7 @@
 import type { Context } from 'hono';
 
 import { isNil } from 'lodash';
+import { buildErrorResponse } from './responseBuilder';
 
 /**
  * 异常响应生成
@@ -35,13 +36,7 @@ export const createErrorResult = (
  */
 export const defaultValidatorErrorHandler = (result: any, c: Context) => {
     if (!result.success) {
-        return c.json(
-            {
-                ...createErrorResult('请求数据验证失败', 400),
-                errors: result.error.format(),
-            },
-            400,
-        );
+        return c.json(buildErrorResponse(createErrorResult('请求数据验证失败', result.error.toString(), 'SERVER_ERROR')), 422);
     }
     return result;
 };
