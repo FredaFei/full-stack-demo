d53a0077fdee3edfdf2d1a52cbb1de8879d21c1a
fredafei <964959033@qq.com>
2025-07-18 13:26:49 +0800

    使用successResponseSchema整理认证相关接口的响应schema

--- chapter14/src/server/auth/schema.ts
+++ chapter14/src/server/auth/schema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 import { extendZodWithOpenApi } from 'zod-openapi';
 
-import { baseMetaSchema } from '../common/schema';
+import { successResponseSchema } from '../common/schema';
 
 extendZodWithOpenApi(z);
 
@@ -12,7 +12,14 @@ export const authLoginRequestBodySchema = z.object({
     credential: z.string().min(1, '用户名或邮箱不能为空').openapi({ description: '用户名或邮箱' }),
     password: z.string().min(8, '密码至少8位').openapi({ description: '用户密码' }),
 });
-
+/**
+ * token数据结构
+ */
+export const tokenSchema = z
+    .object({
+        token: z.string().openapi({ ref: 'Token', description: 'jwt token' })
+    })
+    .openapi({ description: 'token信息' });
 /**
  * 用户数据结构
  */
@@ -29,31 +36,15 @@ export const authSchema = z
 /**
  * 获取用户信息响应数据结构
  */
-export const profileResponseSchema = z
-    .object({
-        data: authSchema.or(z.null()).openapi({ description: '用户信息' }),
-        meta: baseMetaSchema.optional().openapi({ description: '元数据' }),
-    })
-    .openapi({ ref: 'AuthProfile', description: '获取用户信息响应数据' });
+export const profileResponseSchema = successResponseSchema(authSchema.or(z.null())).openapi({ ref: 'AuthProfile', description: '获取用户信息响应数据' });
 
 /**
  * 登录响应数据结构
  */
-export const tokenResponseSchema = z
-    .object({
-        data: z.object({
-            token: z.string().openapi({ ref: 'Token', description: 'jwt token' }),
-            meta: baseMetaSchema.optional().openapi({ description: '元数据' }),
-        }),
-    })
-    .openapi({ ref: 'AuthToken', description: '用户登录/获取token响应数据' });
+export const tokenResponseSchema = successResponseSchema(tokenSchema).openapi({ ref: 'AuthToken', description: '用户登录/获取token响应数据' });
 
 /**
  * 登出响应数据结构
  */
-export const logoutResponseSchema = z
-    .object({
-        data: z.object({ message: z.string() }).openapi({ description: '登出成功消息' }),
-        meta: baseMetaSchema.optional().openapi({ description: '元数据' }),
-    })
-    .openapi({ ref: 'AuthLogout', description: '登出响应数据' });
+export const logoutResponseSchema = successResponseSchema(z.object({ message: z.string() }).openapi({ description: '登出成功消息' }))
+.openapi({ ref: 'AuthLogout', description: '登出响应数据' });
